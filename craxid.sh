83020af122498753e2b84faee97259a761c2350d
Dede Kurniawan <dede.klender@gmail.com>
2023-03-02 17:40:42 +0700

    Create craxid.sh

---
+++ craxid.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+clear && sleep 1 && Memulai instalasi File XFCE dkk... && sleep 1 && print Agak lama kak, Sabar yah! && sleep 1 sudo apt install -y xfce4 desktop-base dbus-x11 xfce4-terminal firefox-esr && wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo apt install ./.deb && rm -r *.deb && sleep 3 && print Done Kak! && print sekarang copas teks yang kamu dapat dari Google Remote Desktop ya!
