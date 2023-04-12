d016e24bdf92aef80cc7358db2402863206f5cce
Dede Kurniawan <dede.klender@gmail.com>
2023-04-12 14:57:36 +0700

    update craxid.sh

--- craxid.sh
+++ craxid.sh
@@ -22,4 +22,22 @@ echo Instalasi XFCE Selesai!
 
 clear
 
-sleep 3 && echo Mendownload Google Remote Desktop... && sleep 3 && wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && clear && sleep 3 && echo Google Remote Desktop selesai didownload! && sleep 2 && echo Menginstal Chrome Remote Desktop && sudo apt install -y ./*.deb && clear && sleep 3 && echo Menghapus sisa Instalasi... && sleep 3 && rm -r chrome-remote* && sleep 3 && echo Done Kak! && sleep 3 && echo sekarang copas teks yang kamu dapat dari Google Remote Desktop ya! && echo Support me! && sleep 2 && echo https://paypal.me/dedeklender
+sleep 3
+
+echo Mendownload Google Remote Desktop...
+
+sleep 3
+
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+
+clear
+
+sleep 3
+
+echo Google Remote Desktop selesai didownload!
+
+sleep 2
+
+echo Menginstal Chrome Remote Desktop
+
+sudo apt install -y ./*.debclear && sleep 3 && echo Menghapus sisa Instalasi... && sleep 3 && rm -r chrome-remote* && sleep 3 && echo Done Kak! && sleep 3 && echo sekarang copas teks yang kamu dapat dari Google Remote Desktop ya! && echo Support me! && sleep 2 && echo https://paypal.me/dedeklender
